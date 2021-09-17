02bac1d092f365de354941a8766f36f5a2add501
luanaselena <luanaselenatraversa@gmail.com>
2021-09-16 23:54:20 -0300

    cors

--- src/main/java/com/unla/kafka/server/KafkaServerApplication.java
+++ src/main/java/com/unla/kafka/server/KafkaServerApplication.java
@@ -2,12 +2,34 @@ package com.unla.kafka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class KafkaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaServerApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+
+			http.csrf().disable()
+					.authorizeRequests()
+					.antMatchers(HttpMethod.POST, "/newUser").permitAll()
+					.antMatchers(HttpMethod.GET, "/login").permitAll()
+					.anyRequest().authenticated();
+			http.cors();
+		}
 	}
 
 }
