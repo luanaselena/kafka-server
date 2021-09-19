2e8264a9d301bba84e20d1be07394b6886996a63
luanaselena <luanaselenatraversa@gmail.com>
2021-09-19 20:42:34 -0300

    endpoint para followers posts

--- src/main/java/com/unla/kafka/server/controller/PostController.java
+++ src/main/java/com/unla/kafka/server/controller/PostController.java
@@ -1,12 +1,17 @@
 package com.unla.kafka.server.controller;
 
+import com.unla.kafka.server.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.unla.kafka.server.producer.PostProducer;
 import com.unla.kafka.server.service.PostService;
+
+import java.util.List;
 
 @RestController
 public class PostController {
@@ -18,10 +23,12 @@ public class PostController {
 	private PostProducer postProducer;
 
 	@PostMapping("/followers-post/{user_id}")
-	public void getFollowersPosts(@PathVariable("user_id") Long userId) {
+	public ResponseEntity<List<Post>> getFollowersPosts(@PathVariable("user_id") Long userId) {
 		var posts = postService.getFollowersPosts(userId);
 
 		postProducer.producePosts(posts);
+
+		return new ResponseEntity<List<Post>>(posts, HttpStatus.OK);
 	}
 
 }
