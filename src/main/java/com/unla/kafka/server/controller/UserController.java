a15ea1882a1a58e8b71b1f4161dccaa778471139
Joel Stella <stella.joelivan@gmail.com>
2021-09-21 01:02:55 -0300

    Save follow

--- src/main/java/com/unla/kafka/server/controller/UserController.java
+++ src/main/java/com/unla/kafka/server/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.unla.kafka.server.controller;
 
-import com.unla.kafka.server.model.User;
-import com.unla.kafka.server.service.UserService;
+import java.util.List;
+
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.unla.kafka.server.consumer.request.FollowRequest;
+import com.unla.kafka.server.model.User;
+import com.unla.kafka.server.producer.UserProducer;
+import com.unla.kafka.server.service.FollowService;
+import com.unla.kafka.server.service.UserService;
 
 @RestController
 @CrossOrigin
@@ -16,6 +25,12 @@ public class UserController {
 
     @Autowired
     UserService userService;
+    
+    @Autowired
+	private FollowService followService;
+    
+    @Autowired
+	private UserProducer userProducer;
 
     @GetMapping("/login")
     public ResponseEntity<Boolean> login(@RequestParam("username") String username, @RequestParam("password") String password) {
@@ -31,5 +46,21 @@ public class UserController {
     @GetMapping("/getUsers")
     public ResponseEntity<List<User>> getAll(){
         return new ResponseEntity<List<User>>(userService.getAll(),HttpStatus.OK);
+    }
+    
+    @PostMapping("/follow")
+    public ResponseEntity<String> saveFollow(
+    		@RequestParam("followingId") Long followingId,
+    		@RequestParam("followerId") Long followerId){
+    	
+    	followService.save(followerId, followingId);
+    	
+    	var followRequest = new FollowRequest();
+    	followRequest.setFollowerId(followerId);
+    	followRequest.setFollowingId(followingId);
+    	
+    	userProducer.produceFollow(followRequest);    	
+    	
+    	return new ResponseEntity<String>(Strings.EMPTY, HttpStatus.OK);
     }
 }
